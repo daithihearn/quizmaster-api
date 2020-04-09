2ce2da4f3b0a6d462a54b5fa53bae2e9c035f26e
Lucia Navaza <admin@yourtechshopper.com>
2020-04-09 20:23:11 +0200

    Fixing environment variables

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 echo "
     Setting up the environment variables"
-export MONGODB_HOSTNAME=mongodb://mongodb:27017
+export MONGODB_URI=mongodb://mongodb:27017
 export MONGODB_DATABASE_NAME=quizmaster
 export PASSWORD_ENCODER_SEED=password
 export JWT_SECRET=password
