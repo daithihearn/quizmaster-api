3665033b451d2da16ea9f5cc1ad1c8b7c9b664da
Daithi Hearn <daithi.hearn@gmail.com>
2020-04-10 10:56:11 +0200

    Model Update Adding imageUri field for storing images.

--- src/main/kotlin/ie/daithi/quizmaster/model/Question.kt
+++ src/main/kotlin/ie/daithi/quizmaster/model/Question.kt
@@ -5,6 +5,7 @@ import ie.daithi.quizmaster.enumeration.QuestionType
 data class Question(
         var index: Int? =null,
         var value: String? = null,
+        var imageUri: String? = null,
         var type: QuestionType? = null,
         var answer: String? = null,
         var options: List<String>? = null
