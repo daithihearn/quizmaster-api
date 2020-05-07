cfbc9796fa66a1a9c25b1b991821ebb6b290a8f9
Daithi Hearn <daithi.hearn@gmail.com>
2020-05-07 19:57:57 +0200

    Enabling Redis

--- build.sh
+++ build.sh
@@ -4,3 +4,12 @@ echo "
 Building quizmaster-api"
 
 ./gradlew build install
+
+if [ -x "$(command -v docker)" ]; then
+    
+    docker build -t localhost:5000/quizmaster-api:latest .
+    docker push localhost:5000/quizmaster-api:latest
+    
+else
+    echo "Docker not installed"
+fi
--- src/main/kotlin/ie/daithi/quizmaster/repositories/CurrentContentRepo.kt
+++ src/main/kotlin/ie/daithi/quizmaster/repositories/CurrentContentRepo.kt
@@ -1,7 +1,7 @@
 package ie.daithi.quizmaster.repositories
 
 import ie.daithi.quizmaster.model.PublishContent
-import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.repository.CrudRepository
 
-interface CurrentContentRepo: MongoRepository<PublishContent, String> {
+interface CurrentContentRepo: CrudRepository<PublishContent, String> {
 }
